c6bdea3811083195c926fc61be4cea40743fa383
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-16 15:10:01 +0900

    if절 병합

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -17,8 +17,13 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
+                if (item.sellIn < 1) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
             } else if (item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality < 50) { // 이 부분 브랜치 커버리지가 여전히 가능한지?
+                if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
                     if (item.sellIn < 11) {
@@ -33,32 +38,30 @@ class GildedRose {
                         }
                     }
                 }
+                if (item.sellIn < 1) {
+                    item.quality = item.quality - item.quality;
+                }
             } else if (item.name.equals(SULFURAS)) {
-            } else if (item.quality > 0) {
-                item.quality = item.quality - 1;
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                if (item.sellIn < 1) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
             }
 
-            if (!item.name.equals(SULFURAS)) {
+            if (item.name.equals(SULFURAS)) {
+            } else {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.name.equals(AGED_BRIE)) {
-                if (item.sellIn < 0) { // 이 부분이 커버 안 됨
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
             } else if (item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
-                }
             } else if (item.sellIn < 0) {
-                if (item.quality > 0) {
-                    if (item.name.equals(SULFURAS)) {
-                    } else {
-                        item.quality = item.quality - 1;
-                    }
-                }
+            } else {
             }
         }
     }
