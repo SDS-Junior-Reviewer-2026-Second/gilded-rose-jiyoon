8917f80ad43d83cc17fd13f3c9400286d95da8c0
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-16 14:49:42 +0900

    merge else if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -33,11 +33,9 @@ class GildedRose {
                         }
                     }
                 }
-            } else {
-                if (item.name.equals(SULFURAS)) {
-                } else if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                }
+            } else if (item.name.equals(SULFURAS)) {
+            } else if (item.quality > 0) {
+                item.quality = item.quality - 1;
             }
 
             if (!item.name.equals(SULFURAS)) {
@@ -50,19 +48,15 @@ class GildedRose {
                         item.quality = item.quality + 1;
                     }
                 }
-            } else {
-                if (item.name.equals(BACKSTAGE_PASSES)) {
-                    if (item.sellIn < 0) {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.sellIn < 0) {
-                        if (item.quality > 0) {
-                            if (item.name.equals(SULFURAS)) {
-                                continue;
-                            }
-                            item.quality = item.quality - 1;
-                        }
+            } else if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+            } else if (item.sellIn < 0) {
+                if (item.quality > 0) {
+                    if (item.name.equals(SULFURAS)) {
+                    } else {
+                        item.quality = item.quality - 1;
                     }
                 }
             }
