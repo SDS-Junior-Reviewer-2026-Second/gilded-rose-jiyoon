18000ce9c26dca43af0f7aa37063af43babf0473
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-16 14:24:56 +0900

    OR condition split하고 필요없는 코드 삭제하기

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,26 +13,27 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             Item item = items[i];
-            if (item.name.equals(AGED_BRIE)
-                    || item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+            if (item.name.equals(AGED_BRIE)) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
+                if (item.quality < 50) { // 이 부분 브랜치 커버리지가 여전히 가능한지?
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn < 11) {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
-
-                            if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
-                                }
-                            }
                         }
-                    } else {
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+            } else {
                 if (item.quality > 0) {
                     if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                         item.quality = item.quality - 1;
